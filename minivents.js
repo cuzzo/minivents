44dee5c7bd36a1ad457c584dcda385eea816c95d
Cuzzo <yahn007@outlook.com>
2014-01-24 22:31:28 -0800

    Terminated lines with semicolons. Added AMD / CommonJS / Window support.

--- minivents.js
+++ minivents.js
@@ -3,30 +3,42 @@
  */
 function Events(target){
   var events = {}, i, list, args, A = Array;
-  target = target || this
+  target = target || this;
     /**
      *  On: listen to events
      */
     target.on = function(type, func, ctx){
-      events[type] || (events[type] = [])
-      events[type].push({f:func, c:ctx})
+      events[type] || (events[type] = []);
+      events[type].push({f:func, c:ctx});
     }
     /**
      *  Off: stop listening to event / specific callback
      */
     target.off = function(type, func){
-      list = events[type] || []
-      i = list.length = func ? list.length : 0
-      while(~--i<0) func == list[i].f && list.splice(i,1)
+      list = events[type] || [];
+      i = list.length = func ? list.length : 0;
+      while(~--i<0) func == list[i].f && list.splice(i,1);
     }
-    /** 
+    /**
      * Emit: send event, callbacks will be triggered
      */
     target.emit = function(){
-      args = A.apply([], arguments)
-      list = events[args.shift()] || []
-      i = list.length
-      for(j=0;j<i;j++) list[j].f.apply(list[j].c, args) 
+      args = A.apply([], arguments);
+      list = events[args.shift()] || [];
+      i = list.length;
+      for(j=0;j<i;j++) list[j].f.apply(list[j].c, args);
     };
 }
-module.exports = Events
+
+if (typeof module === "object") {
+  module.exports = Events;
+}
+else if (typeof window === "object") {
+  window.Events = Events;
+}
+else if (typeof define === "function") {
+  define("minivents", [], function() { return Events; });
+}
+else if (typeof exports === "object") {
+  exports.Events = Events;
+}
